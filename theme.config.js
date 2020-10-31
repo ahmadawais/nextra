6780300e256ada4870a726807fba832020a2d1bb
Iftikhar Ahmad <33150922+driftikharahmad@users.noreply.github.com>
2020-10-31 21:24:22 +0500

    Update theme.config.js

--- theme.config.js
+++ theme.config.js
@@ -18,7 +18,7 @@ export default {
       <meta name="description" content="Nextra: the next site builder" />
       <meta name="og:description" content="Nextra: the next site builder" />
       <meta name="twitter:card" content="summary_large_image" />
-      <meta name="twitter:site" content="@shuding_" />
+      <meta name="twitter:site" content="@mrahmadawais" />
       <meta name="og:title" content="Nextra: the next site builder" />
       <meta name="apple-mobile-web-app-title" content="Nextra" />
     </>
